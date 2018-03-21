a1c09830658ed7153da093678ac7e61075276fbb
Ram_Krishna Shrestha (TSL) <shrestha@nbi.ac.uk>
2018-03-21 10:58:40 +0000

    script modified

--- exome_capture_analysis.py
+++ exome_capture_analysis.py
@@ -16,9 +16,9 @@ for data in  data_files:
 
 
 for sample in sample_set:
-    cmd1="source ruby-2.3.1; rake -f rakefile projectdir=/usr/users/TSL_20/shrestha/workarea/yogesh/exome_capure  sampleid=" + sample + " R1=" + dir_path_to_data + sample + "_L1_1.fq.gz" + " reference=" + reference + " R2=" + dir_path_to_data + sample + "_L1_2.fq.gz" + " trimmomatic:run bbmap:run"
+    cmd1="source ruby-2.3.1; rake -f rakefile projectdir=/usr/users/TSL_20/shrestha/workarea/yogesh/exome_capure  sampleid=" + sample + " R1=" + dir_path_to_data + "/" + sample + "_L1_1.fq.gz" + " reference=" + reference + " R2=" + dir_path_to_data + "/" + sample + "_L1_2.fq.gz" + " trimmomatic:run bbmap:run"
     cmd2=cmd1.replace("_L1_", "_L2_")
-    varscan_cmd="rake -f rakefile projectdir=/usr/users/TSL_20/shrestha/workarea/yogesh/exome_capure  sampleid=" + sample + " reference= " + reference + " bcftools:run"
+    varscan_cmd="rake -f rakefile projectdir=/usr/users/TSL_20/shrestha/workarea/yogesh/exome_capure  sampleid=" + sample + " reference=" + reference + " bcftools:run"
     os.system('sbatch --mem 10G -o ' + sample + '.rakelog ' + ' -J ' + sample + ' --wrap ' + '\"' + cmd1 + "; " + cmd2 + "; " + varscan_cmd + '\"')
     #print "rake --projectdir="."  R1=" + dir_path_to_data + data + "_L2_1.fq.gz" + " R2=" + dir_path_to_data + data + "_L2_2.fq.gz" + " reference=/usr/users/sl/guptay/field_samples/exome_capture/sequence_bait_unique.fasta trimmomatic:run "
 
